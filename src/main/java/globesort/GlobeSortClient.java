af8f14632593250844e02cabb057b5b3f577161c
Yitzchak Blank <blanco7258@gmail.com>
2018-05-09 12:38:26 -0700

    HW2 commit

---
+++ src/main/java/globesort/GlobeSortClient.java
@@ -0,0 +1,122 @@
+package globesort;
+
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import io.grpc.StatusRuntimeException;
+import io.grpc.Status;
+import io.grpc.stub.StreamObserver;
+import net.sourceforge.argparse4j.*;
+import net.sourceforge.argparse4j.inf.*;
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.lang.RuntimeException;
+import java.lang.Exception;
+import java.lang.*;
+import java.util.Arrays;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.CountDownLatch;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+public class GlobeSortClient {
+
+    private final ManagedChannel serverChannel;
+    private final GlobeSortGrpc.GlobeSortBlockingStub serverStub;
+
+	private static int MAX_MESSAGE_SIZE = 100 * 1024 * 1024;
+
+    private String serverStr;
+
+    public GlobeSortClient(String ip, int port) {
+        this.serverChannel = ManagedChannelBuilder.forAddress(ip, port)
+				.maxInboundMessageSize(MAX_MESSAGE_SIZE)
+                .usePlaintext(true).build();
+        this.serverStub = GlobeSortGrpc.newBlockingStub(serverChannel);
+
+        this.serverStr = ip + ":" + port;
+    }
+
+    public void run(Integer[] values) throws Exception {
+        System.out.println("Pinging " + serverStr + "...");
+        long ping_t0 = System.nanoTime(); 
+        serverStub.ping(Empty.newBuilder().build());
+        long ping_t1 = System.nanoTime(); 
+        long latency = ping_t1 - ping_t0;
+        System.out.println("Latency: " + latency);                
+
+        System.out.println("Ping successful.");
+
+        System.out.println("Requesting server to sort array");
+        
+        IntArray request = IntArray.newBuilder().addAllValues(Arrays.asList(values)).build();
+        
+        long appTh_t0 = System.nanoTime();       
+        IntResponse response = serverStub.sortIntegers(request);
+        long appTh_t1 = System.nanoTime(); 
+
+        long appThroughput = appTh_t1 - appTh_t0;        
+        long sortTime = response.value;
+        long oneWayThr = sortTime / 2;
+ 
+        System.out.println("Application Throughput: " + (appTh_t1 - appTh_t2); 
+        System.out.println("Sort Time: " +sortTime); 
+        System.out.println("Network 2-way throughput: " + (appThrouput -
+sortTime); 
+        System.out.println("Network 1-way throughput: " + oneWayThr; 
+ 
+        System.out.println("Sorted array");
+    }
+
+    public void shutdown() throws InterruptedException {
+        serverChannel.shutdown().awaitTermination(2, TimeUnit.SECONDS);
+    }
+
+    private static Integer[] genValues(int numValues) {
+        ArrayList<Integer> vals = new ArrayList<Integer>();
+        Random randGen = new Random();
+        for(int i : randGen.ints(numValues).toArray()){
+            vals.add(i);
+        }
+        return vals.toArray(new Integer[vals.size()]);
+    }
+
+    private static Namespace parseArgs(String[] args) {
+        ArgumentParser parser = ArgumentParsers.newFor("GlobeSortClient").build()
+                .description("GlobeSort client");
+        parser.addArgument("server_ip").type(String.class)
+                .help("Server IP address");
+        parser.addArgument("server_port").type(Integer.class)
+                .help("Server port");
+        parser.addArgument("num_values").type(Integer.class)
+                .help("Number of values to sort");
+
+        Namespace res = null;
+        try {
+            res = parser.parseArgs(args);
+        } catch (ArgumentParserException e) {
+            parser.handleError(e);
+            System.exit(1);
+        }
+        return res;
+    }
+
+    public static void main(String[] args) throws Exception {
+        Namespace cmd_args = parseArgs(args);
+        if (cmd_args == null) {
+            throw new RuntimeException("Argument parsing failed");
+        }
+
+        Integer[] values = genValues(cmd_args.getInt("num_values"));
+
+        GlobeSortClient client = new GlobeSortClient(cmd_args.getString("server_ip"), cmd_args.getInt("server_port"));
+        try {
+            client.run(values);
+        } finally {
+            client.shutdown();
+        }
+    }
+}
